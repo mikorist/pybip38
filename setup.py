6a5ad2e36ef85b7a3bfd1ff14c0a6db11ee745aa
mikorist <31393531+mikorist@users.noreply.github.com>
2023-08-11 19:29:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.11'
+        
     ],
 )
 
