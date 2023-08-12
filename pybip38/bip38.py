02a41ca2e849249f2f027f4ed43e15c65e69dce0
mikorist <31393531+mikorist@users.noreply.github.com>
2023-08-12 20:00:02 +0200

    Update bip38.py

--- pybip38/bip38.py
+++ pybip38/bip38.py
@@ -10,90 +10,170 @@ except ImportError:  pass
 import os
 import sys
 import hashlib
-import unicodedata
-import binascii
 import scrypt
-import gmpy2
+import string
+import unicodedata
 from binascii import hexlify, unhexlify
+from codecs import decode
 from Crypto.Cipher import AES
+import gmpy2
 from gmpy2 import mpz
 
-COMPRESSION_FLAGBYTES = ['20','24','28','2c','30','34','38','3c','e0','e8','f0','f8']
-LOTSEQUENCE_FLAGBYTES = ['04','0c','14','1c','24','2c','34','3c']
-NON_MULTIPLIED_FLAGBYTES = ['c0','c8','d0','d8','e0','e8','f0','f8']
-EC_MULTIPLIED_FLAGBYTES = ['00','04','08','0c','10','14','18','1c','20','24','28','2c','30','34','38','3c']
+def simple_aes_encrypt(msg, key):
+    assert len(key) == 32
+    assert len(msg) == 16
+    msg = hexstrlify(msg)  # Stupid hack/workaround for ascii decode errors
+    msg = msg + '7b7b7b7b7b7b7b7b7b7b7b7b7b7b7b7b'
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.encrypt(unhexlify(msg))[:16]
+
+def simple_aes_decrypt(msg, key):
+    assert len(msg) == 16
+    assert len(key) == 32
+    cipher = AES.new(key, AES.MODE_ECB)
+    msg = hexstrlify(cipher.decrypt(msg))
+    while msg[-2:] == '7b':  # Can't use rstrip for multiple chars
+        msg = msg[:-2]
+    for i in range((32 - len(msg)) // 2):
+        msg = msg + '7b'
+    assert len(msg) == 32
+    return unhexlify(msg)
+
+COMPRESSION_FLAGBYTES = ['20', '24', '28', '2c', '30', '34', '38', '3c', 'e0', 'e8', 'f0', 'f8']
+LOTSEQUENCE_FLAGBYTES = ['04', '0c', '14', '1c', '24', '2c', '34', '3c']
+NON_MULTIPLIED_FLAGBYTES = ['c0', 'c8', 'd0', 'd8', 'e0', 'e8', 'f0', 'f8']
+EC_MULTIPLIED_FLAGBYTES = ['00', '04', '08', '0c', '10', '14', '18', '1c', '20', '24', '28', '2c', '30', '34', '38', '3c']
 N = gmpy2.mpz("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141")
 P = gmpy2.mpz("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
 Gx = gmpy2.mpz("0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798")
 Gy = gmpy2.mpz("0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8")
 
-# Precompute constants
-P_inv = gmpy2.invert(P, N)
-Gx_Gy = (Gx, Gy)
-
 # Memoization for ecmultiply
 ecmultiply_memo = {}
 
 
-def hash160(hexstring):
-    h = hashlib.new("ripemd160")
-    h.update(hashlib.sha256(bytes.fromhex(hexstring)).digest())
-    return h.digest().hex()
-
-
-def hash256(hexstring):
-    return (
-        hashlib.sha256(hashlib.sha256(bytes.fromhex(hexstring)).digest()).digest().hex()
-    )
+def modinv(a, n=P):
+    lm, hm = mpz(1), mpz(0)
+    low, high = a % n, n
+    while low > 1:
+        ratio = high // low
+        nm, new = hm - lm * ratio, high - low * ratio
+        lm, low, hm, high = nm, new, lm, low
+    return lm % n
 
 
 def ecadd(xp, yp, xq, yq):
-    P_inv_xq_xp = (xq - xp) % P
-    P_inv_xq_xp_inv = gmpy2.invert(P_inv_xq_xp, P)
-    m = (yq - yp) * P_inv_xq_xp_inv % P
+    m = ((yq - yp) * modinv(xq - xp, P)) % P
     xr = (m * m - xp - xq) % P
     yr = (m * (xp - xr) - yp) % P
     return xr, yr
 
 
+def ecsubtract(xp, yp, xq, yq):
+    return ecadd(xp, yp, xq, ((P - yq) % P))
+
+
 def ecdouble(xp, yp):
-    ln = 3 * xp * xp % P
-    ld = 2 * yp % P
-    lam = (ln * gmpy2.invert(ld, P)) % P
-    xr = (lam * lam - 2 * xp) % P
+    ln = 3 * xp * xp
+    ld = 2 * yp
+    lam = (ln * modinv(ld, P)) % P
+    xr = (lam**2 - 2 * xp) % P
     yr = (lam * (xp - xr) - yp) % P
     return xr, yr
 
 
 def ecmultiply(xs, ys, scalar):
-    if scalar in ecmultiply_memo:
-        return ecmultiply_memo[scalar]
+    if scalar == 0 or scalar >= N:
+        raise Exception("Invalid scalar.")
 
-    Qx, Qy = Gx_Gy
+    if (xs, ys, scalar) in ecmultiply_memo:
+        return ecmultiply_memo[(xs, ys, scalar)]
 
-    scalar_bin = bin(scalar)[2:]
-    for bit in scalar_bin[1:]:
+    scalarbin = str(bin(scalar)).lstrip("0b")
+    Qx, Qy = xs, ys
+    for i in range(1, len(scalarbin)):
         Qx, Qy = ecdouble(Qx, Qy)
-        if bit == "1":
+        if scalarbin[i] == "1":
             Qx, Qy = ecadd(Qx, Qy, xs, ys)
 
-    ecmultiply_memo[scalar] = (Qx, Qy)
+    ecmultiply_memo[(xs, ys, scalar)] = (Qx, Qy)
     return Qx, Qy
 
 
-def compress(pub):
-    x, y = pub[2:66], pub[66:]
-    y_int = gmpy2.mpz(y, 16)
-    prefix = "03" if y_int & 1 else "02"
-    return prefix + x
+def pow_mod(x, y, z):
+    n = 1
+    while y:
+        if y & 1:
+            n = n * x % z
+        y >>= 1
+        x = x * x % z
+    return n
 
 
-def privtopub(priv, outcompressed=True):
-    priv_int = gmpy2.mpz(priv, 16)
-    x, y = ecmultiply(Gx, Gy, priv_int)
-    x_str, y_str = format(x, "064x"), format(y, "064x")
-    pub = "04" + x_str + y_str
-    return compress(pub) if outcompressed else pub
+def strlify(a):
+    if a == b"b" or a == "b":
+        return "b"
+
+    return str(a).rstrip("'").replace("b'", "", 1).replace("'", "")
+
+
+def isitstring(i):
+    if sys.version_info[0] == 2:
+        if isinstance(i, str) or isinstance(i, unicode) or isinstance(i, basestring):
+            return True
+        else:
+            return False
+    else:
+        if isinstance(i, str):
+            return True
+        else:
+            return False
+
+
+def isitint(i):
+    if sys.version_info[0] == 2:
+        if isinstance(i, int) or isinstance(i, long):
+            return True
+        else:
+            return False
+    else:
+        if isinstance(i, int):
+            return True
+        else:
+            return False
+
+
+def hexstrlify(a):
+    return strlify(hexlify(a))
+
+
+def hexreverse(a):
+    return hexstrlify(unhexlify(a)[::-1])
+
+
+def dechex(num, zfill=0):
+    if isinstance(num, gmpy2.mpz):
+        hex_num = hex(num)[2:]  # Calculate hex representation only once
+
+        # Handle the "0" case without string manipulation
+        if len(hex_num) % 2:
+            hex_num = "0" + hex_num
+
+        bytes_o = bytes.fromhex(hex_num)
+
+        if bytes_o == b"\x00":
+            bytes_o = b"\x00"
+
+        hex_bytes = bytes_o.hex()
+        hex_len = len(hex_bytes)
+        zfill_count = 2 * zfill - hex_len
+
+        if zfill_count > 0:
+            hex_bytes = "0" * zfill_count + hex_bytes
+
+        return hex_bytes
+    else:
+        raise TypeError("Input must be mpz.")
 
 
 def normalize_input(input, preferunicodeoverstring=False, nfconly=False):
@@ -110,6 +190,22 @@ def normalize_input(input, preferunicodeoverstring=False, nfconly=False):
 
     except Exception as e:
         raise Exception("Unable to normalize input:", e)
+
+
+def privtopub(priv, outcompressed=True):
+    x, y = ecmultiply(Gx, Gy, gmpy2.mpz(priv, 16))
+    x = dechex(x, 32)
+    y = dechex(y, 32)
+    o = "04" + x + y
+    if outcompressed:
+        return compress(o)
+    else:
+        return o
+
+
+def multiplypriv(p1, p2):
+    result = (gmpy2.mpz(p1, 16) * gmpy2.mpz(p2, 16)) % N
+    return dechex(result, 32)
 
 
 b58_digits = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
@@ -176,87 +272,64 @@ def b58d(s, check=True):
         return str(hexlify(o)).rstrip("'").replace("b'", "", 1).replace("'", "")
 
 
+def multiplypub(pub, priv, outcompressed=True):
+    if len(pub) == 66:
+        pub = uncompress(pub)
+    x, y = ecmultiply(
+        gmpy2.mpz(pub[2:66], 16), gmpy2.mpz(pub[66:], 16), gmpy2.mpz(priv, 16)
+    )
+    x = dechex(x, 32)
+    y = dechex(y, 32)
+    o = "04" + x + y
+    if outcompressed:
+        return compress(o)
+    else:
+        return o
+
+
+def compress(pub):
+    x = pub[2:66]
+    y = pub[66:]
+    if gmpy2.mpz(y, 16) % 2:
+        o = str("03") + str(x)
+    else:
+        o = str("02") + str(x)
+    return o
+
+
+def uncompress(pub):
+    yp = gmpy2.mpz(pub[:2], 16) - 2
+    x = gmpy2.mpz(pub[2:], 16)
+    a = (pow_mod(x, 3, P) + 7) % P
+    y = pow_mod(a, (P + 1) // 4, P)
+    if y % 2 != yp:
+        y = -y % P
+    x = dechex(x, 32)
+    y = dechex(y, 32)
+    return "04" + x + y
+
+
+def hash160(hexstring):
+    h = hashlib.new("ripemd160")
+    h.update(hashlib.sha256(unhexlify(hexstring)).digest())
+    return str(hexlify(h.digest())).rstrip("'").replace("b'", "", 1).replace("'", "")
+
+
+def hash256(hexstring):
+    return (
+        str(
+            hexlify(
+                hashlib.sha256(hashlib.sha256(unhexlify(hexstring)).digest()).digest()
+            )
+        )
+        .rstrip("'")
+        .replace("b'", "", 1)
+        .replace("'", "")
+    )
+
+
 def pubtoaddress(pub, prefix="00"):
     return b58e(prefix + hash160(pub))
-
-
-def simple_aes_encrypt(msg, key):
-    assert len(key) == 32
-    assert len(msg) == 16
-    msg = hexstrlify(msg)  # Stupid hack/workaround for ascii decode errors
-    msg = msg + "7b7b7b7b7b7b7b7b7b7b7b7b7b7b7b7b"
-    cipher = AES.new(key, AES.MODE_ECB)
-    return cipher.encrypt(unhexlify(msg))[:16]
-
-
-def simple_aes_decrypt(msg, key):
-    assert len(msg) == 16
-    assert len(key) == 32
-
-    cipher = AES.new(key, AES.MODE_ECB)
-    decrypted_msg = hexlify(cipher.decrypt(msg)).decode("utf-8")
-
-    # Remove trailing "7b" characters
-    while decrypted_msg.endswith("7b"):
-        decrypted_msg = decrypted_msg[:-2]
-
-    # Pad with "7b" characters if necessary
-    padding_length = (32 - len(decrypted_msg)) // 2
-    padding = "7b" * padding_length
-    decrypted_msg += padding
-
-    assert len(decrypted_msg) == 32
-    return unhexlify(decrypted_msg)
-
-
-def dechex(num, zfill=0):
-    if isinstance(num, gmpy2.mpz):
-        hex_num = format(num, "x")  # Convert integer to hexadecimal string directly
-
-        if len(hex_num) % 2:
-            hex_num = "0" + hex_num
-
-        hex_len = len(hex_num)
-        zfill_count = 2 * zfill - hex_len
-
-        if zfill_count > 0:
-            hex_num = "0" * zfill_count + hex_num
-
-        return hex_num
-    else:
-        raise TypeError("Input must be mpz.")
-
-
-def multiplypriv(p1, p2):
-    result = (gmpy2.mpz(p1, 16) * gmpy2.mpz(p2, 16)) % N
-    return dechex(result, 32)
-
-
-def strlify(a):
-    if a == b"b" or a == "b":
-        return "b"
-    return str(a).rstrip("'").replace("b'", "", 1).replace("'", "")
-
-
-def hexstrlify(a):
-    return strlify(hexlify(a))
-
-
-def wiftohex(wifkey):
-    iscompressed = False
-    wifkey = normalize_input(wifkey)
-    assert len(wifkey) == 50 or len(wifkey) == 51 or len(wifkey) == 52
-    for c in wifkey:
-        if c not in b58_digits:
-            raise Exception("Not WIF")
-    key = b58d(wifkey)
-    prefix, key = key[:2], key[2:]
-    if len(key) == 66:
-        assert key[-2:] == "01"
-        key = key[:-2]
-        iscompressed = True
-    assert len(key) == 64
-    return key, prefix, iscompressed
 
 
 def bip38decrypt(password, encrypted_private_key, outputlotsequence=False):
@@ -396,6 +469,53 @@ def intermediate_code(
     return b58e(magicbytes + owner_entropy + passpoint)
 
 
+def privtohex(key):
+    if isitint(key):
+        key = dechex(key, 32)
+    else:
+        try:
+            key, z, zz = wiftohex(key)
+            assert len(key) == 64
+        except:
+            try:
+                key = unhexlify(key)
+            except:
+                try:
+                    key1 = hexstrlify(key)
+                    assert len(key1) == 64 or len(key1) == 66 or len(key1) == 68
+                    if len(key1) == 68:
+                        assert key1[-2:] == "01"
+                    key = key1
+                except:
+                    raise Exception("Cannot interpret input key.")
+            else:
+                key = hexstrlify(key)
+    if len(key) == 68:
+        assert key[-2:] == "01"
+        key = key[:-2]
+    if len(key) == 66:
+        key = key[2:]
+    assert len(key) == 64
+    return key
+
+
+def wiftohex(wifkey):
+    iscompressed = False
+    wifkey = normalize_input(wifkey)
+    assert len(wifkey) == 50 or len(wifkey) == 51 or len(wifkey) == 52
+    for c in wifkey:
+        if c not in b58_digits:
+            raise Exception("Not WIF")
+    key = b58d(wifkey)
+    prefix, key = key[:2], key[2:]
+    if len(key) == 66:
+        assert key[-2:] == "01"
+        key = key[:-2]
+        iscompressed = True
+    assert len(key) == 64
+    return key, prefix, iscompressed
+
+
 def bip38encrypt(password, priv, iscompressed=False):
     password = normalize_input(password, False, True)
     try:
@@ -487,81 +607,49 @@ def passphrase_to_key(intermediatecode, iscompressed=False, seedb=os.urandom(24)
 
     return enckey, cfrm38code, address
 
-def pow_mod(x,y,z):
-    n = 1
-    while y:
-        if y & 1:
-            n = n * x % z
-        y >>= 1
-        x = x * x % z
-    return n
 
-def multiplypub(pub,priv,outcompressed=True):
-    if len(pub) == 66:
-        pub = uncompress(pub)
-    x, y = ecmultiply(gmpy2.mpz(pub[2:66],16),gmpy2.mpz(pub[66:],16),gmpy2.mpz(priv,16))
-    x = dechex(x,32)
-    y = dechex(y,32)
-    o = '04' + x + y
-    if outcompressed:
-        return compress(o)
-    else:
-        return o
-
-
-def compress(pub):
-    x = pub[2:66]
-    y = pub[66:]
-    if gmpy2.mpz(y,16) % 2:
-        o = str('03') + str(x)
-    else:
-        o = str('02') + str(x)
-    return o
-
-
-def uncompress(pub):
-    yp = gmpy2.mpz(pub[:2],16) - 2
-    x = gmpy2.mpz(pub[2:],16)
-    a = (pow_mod(x,3,P) + 7) % P
-    y = pow_mod(a, (P+1)//4, P)
-    if y % 2 != yp:
-        y = -y % P
-    x = dechex(x,32)
-    y = dechex(y,32)
-    return '04' + x + y
-
-
-def confirm38code(password, cfrm38code):
+def confirm38code(password, cfrm38code, outputlotsequence=False):
     password = normalize_input(password, False, True)
-    cfrm38code = b58d(cfrm38code)  # Convert from Base58 to bytes
+    cfrm38code = b58d(cfrm38code)
     assert len(cfrm38code) == 102
     assert cfrm38code[:10] == "643bf6a89a"
-    
     flagbyte = cfrm38code[10:12]
     addresshash = cfrm38code[12:20]
     owner_entropy = cfrm38code[20:36]
     encpointb = cfrm38code[36:]
 
-    owner_salt = owner_entropy[:8]
-    lotsequence = owner_entropy[8:]
-
-    salt = unhexlify(owner_salt)
-    prefactor = hexstrlify(scrypt.hash(password, salt, 16384, 8, 8, 32))
-
-    if lotsequence:
-        passfactor = hash256(prefactor + owner_entropy)
+    if flagbyte in LOTSEQUENCE_FLAGBYTES:
+        owner_salt = owner_entropy[:8]
+        lotsequence = owner_entropy[8:]
     else:
-        passfactor = prefactor
+        lotsequence = False
+        owner_salt = owner_entropy
+
+    owner_salt = unhexlify(owner_salt)
+    prefactor = hexstrlify(scrypt.hash(password, owner_salt, 16384, 8, 8, 32))
+    passfactor = (
+        hash256(prefactor + owner_entropy)
+        if flagbyte in LOTSEQUENCE_FLAGBYTES
+        else prefactor
+    )
+
+    passfactor_mmpz = gmpy2.mpz(passfactor, 16)
+    if passfactor_mmpz == 0 or passfactor_mmpz >= N:
+        if outputlotsequence:
+            return (False, False, False) if lotsequence else False
+        else:
+            return False
 
     passpoint = privtopub(passfactor, True)
     password = unhexlify(passpoint)
-    
     salt = unhexlify(addresshash + owner_entropy)
     scrypthash = hexstrlify(scrypt.hash(password, salt, 1024, 1, 1, 64))
+    scrypthash_prefix = scrypthash[:32]
+    scrypthash_suffix = scrypthash[32:64]
+
     msg1 = unhexlify(encpointb[2:34])
     msg2 = unhexlify(encpointb[34:])
     key = unhexlify(scrypthash[64:])
-
     half1 = simple_aes_decrypt(msg1, key)
     half2 = simple_aes_decrypt(msg2, key)
     half1, half2 = hexstrlify(half1), hexstrlify(half2)
@@ -569,23 +657,31 @@ def confirm38code(password, cfrm38code):
     pointb_half1 = gmpy2.mpz(half1, 16) ^ gmpy2.mpz(scrypthash[:32], 16)
     pointb_half2 = gmpy2.mpz(half2, 16) ^ gmpy2.mpz(scrypthash[32:64], 16)
     pointb_xcoord = dechex(pointb_half1, 16) + dechex(pointb_half2, 16)
-
     pointb_prefix = gmpy2.mpz(encpointb[:2], 16) ^ (gmpy2.mpz(scrypthash[126:], 16) & 1)
     pointb = dechex(pointb_prefix, 1) + pointb_xcoord
-
     newkey = multiplypub(pointb, passfactor, False)
 
     if flagbyte in COMPRESSION_FLAGBYTES:
         newkey = compress(newkey)
 
     address = pubtoaddress(newkey, "00")
-    addrhex = hexstrlify(bytearray(address, "ascii"))
+    addrhex = (
+        hexstrlify(address)
+        if isinstance(address, bytes)
+        else hexstrlify(bytearray(address, "ascii"))
+    )
     addresshash2 = hash256(addrhex)[:8]
 
     if addresshash == addresshash2:
-        return address
+        if outputlotsequence and lotsequence is not False:
+            lotsequence = gmpy2.mpz(lotsequence, 16)
+            sequence = lotsequence % 4096
+            lot = (lotsequence - sequence) // 4096
+            return address, lot, sequence
+        else:
+            return address if not outputlotsequence else (address, False, False)
     else:
-        return False
+        return (False, False, False) if outputlotsequence else False
 
 
 def addversion(encpriv, version="80"):
